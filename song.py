1306fba1a2687e21231acaf6bae4e3f58c3d755a
Daria Pionk <dariapionk13@gmail.com>
2021-11-16 19:42:22 +0100

    Multiple verses all cases ok

--- song.py
+++ song.py
@@ -15,7 +15,7 @@ class Song:
 "On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree./n",
 "On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree./n"]
 		songToGet = ""
-		if isinstance(arg, list) and (len(arg) != 2 or arg[1] <= arg[0] or arg[0] < 1 or arg[1] > 12):
+		if isinstance(arg, list) and (len(arg) != 2 or not isinstance(arg[0], int) or not isinstance(arg[1],int) or arg[1] <= arg[0] or arg[0] < 1 or arg[1] > 12 ):
 			raise ValueError("Zły argument")
 		if isinstance(arg, str) and arg != "whole":
 			raise ValueError("Zły argument")
@@ -26,7 +26,7 @@ class Song:
 				songToGet += song[i - 1]
 		if arg == "whole":
 			for i in song:
-				songToGet += i		
+				songToGet += i	
 		return songToGet
 
 getSong=Song().getSong
@@ -135,39 +135,39 @@ class SongTest(unittest.TestCase):
 	def test_disallow_stop_higher_than_12_2(self):
 		with self.assertRaisesWithMessage(ValueError):
 			getSong([4,1212])
-	@unittest.skip("Not done")
+
 	def test_disallow_start_and_stop_incorrect(self):
 		with self.assertRaisesWithMessage(ValueError):
 			getSong([-345,1212435435])
-	@unittest.skip("Not done")
+
 	def test_disallow_start_and_stop_incorrect_2(self):
 		with self.assertRaisesWithMessage(ValueError):
 			getSong([150, -45645])
-	@unittest.skip("Not done")
+
 	def test_disallow_start_and_stop_incorrect_3(self):
 		with self.assertRaisesWithMessage(ValueError):
 			getSong([150, {}])
-	@unittest.skip("Not done")
+
 	def test_disallow_start_and_stop_incorrect_4(self):
 		with self.assertRaisesWithMessage(ValueError):
-			getSong([150, {}])
-	@unittest.skip("Not done")
+			getSong([456456, True])
+
 	def test_disallow_start_and_stop_incorrect_5(self):
 		with self.assertRaisesWithMessage(ValueError):
 			getSong([[], ()])
-	@unittest.skip("Not done")
+
 	def test_disallow_start_and_stop_incorrect_6(self):
 		with self.assertRaisesWithMessage(ValueError):
 			getSong([4.5, 4564564564.120])
-	@unittest.skip("Not done")
+
 	def test_disallow_start_ok_and_stop_incorrect(self):
 		with self.assertRaisesWithMessage(ValueError):
 			getSong([3, True])
-	@unittest.skip("Not done")
+
 	def test_disallow_stop_ok_and_start_incorrect_2(self):
 		with self.assertRaisesWithMessage(ValueError):
 			getSong([None, 11])
-	@unittest.skip("Not done")
+
 	def test_disallow_stop_ok_and_start_incorrect_3(self):
 		with self.assertRaisesWithMessage(ValueError):
 			getSong([3.5, 11])
